e14ea03a4c81e1c143a7af4bf0c704fa1108175b
Parth <parthsanjay2004@gmail.com>
2025-04-15 18:32:35 +0530

    eigth commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Nav from "./components/nav";
 import SignupModal from "./components/SignupModal";
+import Image from 'next/image';
 
 export default function Home() {
   const [showSignup, setShowSignup] = useState(false);
@@ -25,7 +26,7 @@ export default function Home() {
             INVESTING AT YOUR FINGERTIPS
           </div>
         </div>
-        <div className="text-black text-center my-7 text-2xl font-normal">
+        <div className="text-zinc-800 text-center my-7 text-2xl font-normal">
           <div>Seamlessly connect with mutual fund</div>
           <div>Distributors, exploring top-performing</div>
           <div>funds, and track your investments-all</div>
@@ -95,15 +96,17 @@ export default function Home() {
             <p className="text-center">Rated 4.7+ on Playstore and</p>
             <p className="text-center">Appstore</p></div>
         </div>
-        <div className="flex space-x-25 mt-40 mb-40">
-          <div className="w-[50%] h-auto space-y-8 ml-12">
-            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold">Build Wealth the Smart Way with Mutual Funds</h1>
-            <div className="mt-5 justify-center">
+        <div className="md:flex lg:flex sm:flex space-x-25 mt-40 mb-40">
+          <div className="max-w-[50%] h-auto space-y-8 ml-12 max-sm:max-w-[100%] max-sm:justify-items-center">
+
+
+            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold max-sm:text-center">Build Wealth the Smart Way with Mutual Funds</h1>
+            <div className="mt-5">
               <p className="text-lg text-zinc-800">True wealth creation doesn’t happen overnight—it’s the result of careful</p>
               <p className="text-lg text-zinc-800">planning,emotional discipline,and consistent execution.At Askyourexpert,</p>
               <p className="text-lg text-zinc-800">you every step of the way,helping you turn your financial dreams into reality.</p>
 
-             
+
             </div>
             <div className="space-y-3 mt-15">
 
@@ -121,50 +124,50 @@ export default function Home() {
 
           </div>
           <div className="w-[50%] h-auto">
-            <img src="https://www.assetplus.in/assets/drawables/howitworks@2x.png" className="md:w-145 md:h-120 ml-10 mt-10 sm:w-[50%] sm:h-130 w-150 h-140"></img>
+            <img src="https://www.assetplus.in/assets/drawables/howitworks@2x.png" className="md:w-150 md:h-130 ml-10 mt-10 sm:w-140 sm:h-120 lg:w-160 lg:h-150 max-sm:hidden"></img>
           </div>
         </div>
-        <div className="flex gap-x-30 mt-10 mb-5">
-          <div className="w-[50%] h-auto ml-12">
-            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold">What is ASKYUREXPERT?</h1>
+        <div className="md:flex lg:flex sm:flex gap-x-30 mt-10 mb-5">
+          <div className="max-w-[50%] h-auto ml-12 max-sm:max-w-[100%]">
+            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold text-center">What is ASKYUREXPERT?</h1>
             <div className="mt-7 space-y-1">
-              <p className="text-lg">Askyurexpert is a dynamic online platform designed to connect you with a diverse network of verified experts across various industries and disciplines.Whether 
-               </p>
-                <p className="text-lg"> you are facing a complex technical issue,seeking professional advice, or need</p>
-                
-              <p className="text-lg">guidance on a specific project,Askyurexpert provides a seamless and </p>
-              <p className="text-lg">efficient way to get the answers you need. Simply ask your question and let our community of experienced professionals share their knowledge and insights.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Askyurexpert is a dynamic online platform designed to connect you with a diverse network of verified experts across various industries and disciplines.
+              </p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Whether you are facing a complex technical issue,seeking professional advice, or need</p>
+
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">guidance on a specific project,Askyurexpert provides a seamless and </p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">efficient way to get the answers you need. Simply ask your question and let our community of experienced professionals share their knowledge and insights.</p>
             </div>
           </div>
           <div className="w-[50%] justify-end h-auto">
-            <img src="https://personalfinancelab.com/wp-content/uploads/distracting-students-grab-attention.png" alt="Students grabbing attention" className="md:w-140 md:h-100 sm:w-160 sm:h-120 w-350 h-120"></img>
+            <img src="https://personalfinancelab.com/wp-content/uploads/distracting-students-grab-attention.png" className="md:w-140 md:h-100 sm:w-140 sm:h-120 lg:w-160 lg:h-140 max-sm:hidden"></img>
           </div>
         </div>
-        <div className="flex mt-5 mb-50 gap-x-20">
-          <div className="w-[50%] h-auto ml-12">
+        <div className="md:flex lg:flex sm:flex mt-5 mb-50 gap-x-20">
+          <div className="max-w-[50%] h-auto ml-12 max-sm:max-w-[100%]">
 
-            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold">How it works?</h1>
+            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold text-center">How it works?</h1>
             <div className="mt-7 mb-10 space-y-2">
-              <p className="text-lg">Clearly and concisely describe your query. Provide relevant details to</p>
-              <p className="text-lg">help experts understand your needs.</p>
-              <p className="text-lg">Your question is routed to relevant experts within our network.</p>
-              <p className="text-lg">Review the responses and choose the insights that best address your question.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Clearly and concisely describe your query. Provide relevant details to</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">help experts understand your needs.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Your question is routed to relevant experts within our network.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Review the responses and choose the insights that best address your question.</p>
             </div>
           </div>
 
-          <div>
-            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold">Key Features & Benefits</h1>
+          <div className="max-w-[50%] max-sm:max-w-[100%] h-auto ml-12">
+            <h1 style={{ fontFamily: "CrimsonText" }} className="text-3xl font-bold text-center">Key Features & Benefits</h1>
             <div className="mt-7 mb-10 space-y-2">
-              <p className="text-lg">Connect with professionals from various fields, ensuring you find</p>
-              <p className="text-lg">the right expertise for your specific needs.</p>
-              <p className="text-lg">Get tailored answers directly relevant to your challenges, eliminating irrelevant information.</p>
-              <p className="text-lg">Stop sifting through countless articles and forums.Get direct answers quickly and efficiently.</p>
-              <p className="text-lg">Benefit from the practical experience and in-depth knowledge of seasoned professionals.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Connect with professionals from various fields, ensuring you find</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">the right expertise for your specific needs.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Get tailored answers directly relevant to your challenges, eliminating irrelevant information.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Stop sifting through countless articles and forums.Get direct answers quickly and efficiently.</p>
+              <p className="font-light md:text-lg lg:text-lg text-zinc-800">Benefit from the practical experience and in-depth knowledge of seasoned professionals.</p>
             </div>
           </div>
 
         </div>
-        
+
 
 
       </main>
@@ -172,8 +175,8 @@ export default function Home() {
       <footer style={{ backgroundColor: "#7ED1ED" }} className="w-full h-auto">
 
         {/* <div className="w-full h-[86px] bg-blue-300"></div> */}
-        <div className="grid grid-cols-2 sm:gap-4">
-          <div className="space-y-6">
+        <div className="sm:grid sm:grid-cols-2 sm:gap-4 flex flex-col">
+          <div className="space-y-6 sm:order-1 order-2 ml-5 mt-2">
             <div style={{ fontFamily: '"Crimson Text"' }}
               className="text-2xl text-zinc-800 font-bold">ASKYUREXPERT</div>
             <div className="text-sm text-zinc-800 font-extralight my-2">
@@ -187,7 +190,7 @@ export default function Home() {
               <p>indicative of future performance</p>
             </div>
           </div>
-          <div className="grid grid-cols-3 md:gap-10 sm:gap-7 lg:gap-15 ">
+          <div className="order-1 sm:order-2 grid grid-cols-3 mb-5 mt-2 ml-5 sm:grid-cols-3 md:gap-10 sm:gap-7 lg:gap-15 sm:space-y-2">
             <div className="space-y-1">
               <div className="text-zinc-800">Legal</div>
               <div className="text-blue-700 cursor-pointer">Privacy Policy</div>
